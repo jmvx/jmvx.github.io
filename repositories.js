971d4915b153767ffa92dbd661cbdd3305f4fac6
Julia Van Cleve <julia@jmv.me>
2015-05-31 15:59:03 -0700

    github landing page to list repositories

---
+++ repositories.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  
+  $.ajax({
+    type: "GET",
+    url: "https://api.github.com/users/jmvx/repos",
+    dataType: "json",
+    success: function(result) {
+      for (i in result) {
+        url = result[i].html_url;
+        name = result[i].name;
+        description = result[i].description;
+        $("div#myprojects").append("<a href='" + url + "' target='_blank'><div class='project'> <div class='name'>" + name + "</div><div class='description'>" + description + "</div></div></a>");
+      }
+      
+      $(".project:even").addClass('left-col');
+      $(".project:odd").addClass('right-col');
+      
+      
+    }
+  });
+  
+});
